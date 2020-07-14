90fe5ba15aee6b9decd9fcb6fe7e00cec6ed4634
Anthony Dito <anthonymdito@gmail.com>
2020-07-14 16:07:36 -0700

    Added in the method to send the blogs

--- App.js
+++ App.js
@@ -1,16 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, ScrollView, TextInput } from 'react-native';
+import { StyleSheet, Text, ScrollView, TextInput, Button } from 'react-native';
 
 export default function App() {
 
   const [text, setText] = React.useState("");
+
+  const onSubmit = () => {
+    var blogSaving = {
+      text: text
+    };
+    fetch("http://localhost:8080/create-blog", {
+      method: "POST",
+      body: JSON.stringify(blogSaving),
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+    })
+    .then((data) => data.json())
+    .then((blogs) => {
+    })
+  };
 
   return (
     <ScrollView style={styles.container}>
       <Text>Your Blog</Text>
       <Text>These are you most recent posts</Text>
       <TextInput style={styles.textInput} value={text} onChangeText={setText} />
+      <Button onPress={onSubmit} title="Submit" />
       <StatusBar style="auto" />
     </ScrollView>
   );
