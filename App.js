93fb9adc469c3f342f63c2863afa6f4d40645c91
Anthony Dito <anthonymdito@gmail.com>
2020-07-14 15:46:50 -0700

    Adds a text input for the user to type blog posts

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, ScrollView } from 'react-native';
+import { StyleSheet, Text, ScrollView, TextInput } from 'react-native';
 
 export default function App() {
+
+  const [text, setText] = React.useState("");
+
   return (
     <ScrollView style={styles.container}>
       <Text>Your Blog</Text>
       <Text>These are you most recent posts</Text>
+      <TextInput style={styles.textInput} value={text} onChangeText={setText} />
       <StatusBar style="auto" />
     </ScrollView>
   );
@@ -18,5 +22,10 @@ const styles = StyleSheet.create({
     paddingRight: 20,
     paddingBottom: 20,
     paddingTop: 60,
+  },
+  textInput: {
+    padding: 8,
+    borderColor: "black",
+    borderWidth: 1
   }
 });
